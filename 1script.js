a0eb2c5d03ac8eb188907c288cb309b21d15367a
Gauravmohadikar <42213837+Gauravmohadikar@users.noreply.github.com>
2020-02-02 13:05:19 +0530

    Add files via upload

---
+++ 1script.js
@@ -0,0 +1,9 @@
+var firstName = prompt("What is your first name ?") ;
+var lastName = prompt("What is your last name ?") ;
+var age = prompt("What is your age ?") ;
+var days = age * 365 ;
+alert("You have been alive from " + days + " days") ;
+alert("Welcome to Museum Of Candy") ;
+
+console.log("Your Fullname is " + firstName + " " + lastName) ;
+console.log("Your age is " + age) ;
